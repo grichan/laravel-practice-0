1310845d0d65aad10682c1248ce6a139ea5ad6d2
Vlad <vladislav.grichanliyski@gmail.com>
2021-01-14 15:43:10 +0000

    added tailwingcss

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body class="bg-gray-100">
+    <nav class="p-6 bg-white flex justify-between">
+        <ul class="flex items-center">
+            <li>
+                <a href="">Home</a>
+            </li>
+            <li>
+                <a href="">Dashboard</a>
+            </li>
+            <li>
+                <a href="">Post</a>
+            </li>
+        </ul>
+    </nav>
+
+    @yield('content')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     //
-    return view('posts.index');
+    return view('index');
 });
